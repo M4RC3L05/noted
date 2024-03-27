00742288867fad3038949f01ef64f4c9bf0a1f96
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-03-27 22:21:58 +0000

    Add basic api
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ src/apps/api/app.ts
@@ -0,0 +1,140 @@
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { CustomDatabase } from "../../database/mod.ts";
+import { sql } from "@m4rc3l05/sqlite-tag";
+import { makeLogger } from "../../common/logger/mod.ts";
+import { z } from "zod";
+
+declare module "hono" {
+  interface ContextVariableMap {
+    shutdown: AbortSignal;
+    db: CustomDatabase;
+  }
+}
+
+const log = makeLogger("app");
+
+export const makeApp = (
+  { db, signal }: { db: CustomDatabase; signal: AbortSignal },
+) => {
+  const app = new Hono();
+
+  app.onError((error, c) => {
+    log.error("Error while handling request", { error });
+
+    return c.json({
+      error: { code: "internal-server-error", message: "Something went wrong" },
+    }, 500);
+  });
+
+  app.notFound((c) =>
+    c.json({ error: { code: "not-found", message: "Not found" } }, 404)
+  );
+
+  app.use((c, next) => {
+    c.set("shutdown", signal);
+    c.set("db", db);
+
+    return next();
+  });
+
+  app.get("/api/notes", (c) => {
+    const { includeDeleted } = z.object({
+      includeDeleted: z.string().optional(),
+    }).strict().parse(c.req.query());
+
+    const notes = c.get("db").all(
+      sql`
+        select
+          id, name, created_at, updated_at ${
+        sql.if(typeof includeDeleted === "string", () => sql`,deleted_at`)
+      }
+        from notes
+        ${
+        sql.if(typeof includeDeleted !== "string", () =>
+          sql`where deleted_at is null`)
+      }
+        order by updated_at desc`,
+    );
+
+    return c.json({ data: notes });
+  });
+
+  app.get("/api/notes/:id", (c) => {
+    const { id } = z.object({ id: z.string() }).strict().parse(c.req.param());
+
+    const note = c.get("db").get(sql`select * from notes where id = ${id}`);
+
+    if (!note) {
+      throw new HTTPException(404, { message: "Could not find note" });
+    }
+
+    return c.json({ data: note });
+  });
+
+  app.post("/api/notes", async (c) => {
+    const { content, name } = z.object({
+      name: z.string(),
+      content: z.string().optional(),
+    })
+      .strict().parse(await c.req.json());
+
+    const note = c.get("db").get(sql`
+      insert into notes ${sql.insert({ content, name })}
+      returning *;
+    `);
+
+    return c.json({ data: note }, 201);
+  });
+
+  app.patch(
+    "/api/notes/:id",
+    async (c) => {
+      const { id } = z.object({ id: z.string() }).strict().parse(c.req.param());
+      const { content, name, delete: del } = z.object({
+        name: z.string().optional(),
+        content: z.string().optional(),
+        delete: z.boolean().optional(),
+      })
+        .strict().superRefine((data, ctx) => {
+          if (Object.keys(data).length <= 0) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: "At least one prop must be provided",
+            });
+          }
+        }).parse(await c.req.json());
+
+      const note = c.get("db").get(sql`
+        update notes
+        set
+          ${sql.if(typeof name === "string", () => sql`name = ${name}`)}
+          ${
+        sql.if(typeof content === "string", () => sql`content = ${content}`)
+      }
+          ${
+        sql.if(typeof del === "boolean", () =>
+          sql`deleted_at = ${del ? new Date().toISOString() : null}`)
+      }
+        where id = ${id}
+        returning *;
+      `);
+
+      return c.json({ data: note });
+    },
+  );
+
+  app.delete("/api/notes/:id", (c) => {
+    const { id } = z.object({ id: z.string() }).strict().parse(c.req.param());
+
+    const result = c.get("db").execute(sql`delete from notes where id = ${id}`);
+
+    if (result <= 0) {
+      throw new HTTPException(404, { message: "Could not find note" });
+    }
+
+    return c.body(null, 204);
+  });
+
+  return app;
+};
---
+++ src/apps/api/main.ts
@@ -0,0 +1,50 @@
+import { HookDrain } from "../../common/process/hook-drain.ts";
+import { makeLogger } from "../../common/logger/mod.ts";
+import { gracefulShutdown } from "../../common/process/mod.ts";
+import { makeDatabase } from "../../database/mod.ts";
+import { makeApp } from "./app.ts";
+
+const log = makeLogger("api");
+const shutdown = new HookDrain({
+  log,
+  onFinishDrain: (error) => {
+    log.info("Exiting application");
+
+    if (error.error) {
+      if (error.reason === "timeout") {
+        log.warn("Global shutdown timeout exceeded");
+      }
+
+      Deno.exit(1);
+    } else {
+      Deno.exit(0);
+    }
+  },
+});
+
+gracefulShutdown({ hookDrain: shutdown, log });
+
+const db = makeDatabase();
+const app = makeApp({ db, signal: shutdown.signal });
+
+const server = Deno.serve({
+  hostname: "0.0.0.0",
+  port: 4321,
+  onListen: ({ hostname, port }) => {
+    log.info(`Serving on http://${hostname}:${port}`);
+  },
+}, app.fetch);
+
+shutdown.registerHook({
+  name: "api",
+  fn: async () => {
+    await server.shutdown();
+  },
+});
+
+shutdown.registerHook({
+  name: "db",
+  fn: () => {
+    db.close();
+  },
+});
