6624661260018ac2402a0e58187d41221e89d585
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-03-27 22:21:48 +0000

    Add process utils
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ src/common/process/mod.ts
@@ -0,0 +1,26 @@
+import { Logger } from "@std/log";
+import { HookDrain } from "./hook-drain.ts";
+
+export const gracefulShutdown = (
+  { hookDrain, log }: { hookDrain: HookDrain; log: Logger },
+) => {
+  for (const signal of ["SIGABRT", "SIGTERM", "SIGINT"] as Deno.Signal[]) {
+    Deno.addSignalListener(signal, () => {
+      log.info(`OS Signal "${signal}" captured`);
+
+      hookDrain.drain();
+    });
+  }
+
+  globalThis.addEventListener("unhandledrejection", (e) => {
+    log.error("Unhandled rejection captured", { reason: e.reason });
+
+    hookDrain.drain();
+  });
+
+  globalThis.addEventListener("error", (e) => {
+    log.error("Unhandled error captured", { error: e.error });
+
+    hookDrain.drain();
+  });
+};
