9ea01c0ee512b8939d22de7e18a6931bd875debc
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-05-07 22:15:23 +0100

    Add @m4rc3l05/requester to replace fetch calls directly
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/web/services/base-service.ts
+++ src/apps/web/services/base-service.ts
@@ -1,19 +1,25 @@
 import { deepMerge } from "@std/collections";
 import { encodeBase64 } from "@std/encoding/base64";
+import { Requester } from "@m4rc3l05/requester";
+import * as requesterComposers from "@m4rc3l05/requester/composers";
 
 export abstract class BaseService {
   #baseUrl: string;
   #auth: { username: string; password: string };
+  #requester: ReturnType<Requester["build"]>;
 
   constructor(baseUrl: string, auth: { username: string; password: string }) {
     this.#baseUrl = baseUrl;
     this.#auth = auth;
+    this.#requester = new Requester().with(
+      requesterComposers.timeout({ ms: 10000 }),
+    ).build();
   }
 
   request(
     { path, init }: { path: string; init?: Parameters<typeof fetch>[1] },
   ) {
-    return fetch(
+    return this.#requester(
       `${this.#baseUrl}${path}`,
       // deno-lint-ignore no-explicit-any
       deepMerge((init ?? {}) as any, {
