1515eb1438a5d7d4fd2f9748d280ad8f82706483
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-08-24 00:39:03 +0100

    Remove db optimise cron
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/api/main.ts
+++ src/apps/api/main.ts
@@ -4,7 +4,6 @@ import { type CustomDatabase, makeDatabase } from "#src/database/mod.ts";
 import { makeApp } from "#src/apps/api/app.ts";
 import { ProcessLifecycle } from "@m4rc3l05/process-lifecycle";
 import config from "config";
-import { Cron } from "@m4rc3l05/cron";
 
 const log = makeLogger("api");
 const { host, port } = config.get("apps.api");
@@ -22,39 +21,6 @@ processLifecycle.registerService({
 
     db.close();
   },
-});
-
-processLifecycle.registerService({
-  name: "db-optimise",
-  boot: (pl) => {
-    const db = pl.getService<CustomDatabase>("db");
-    const cronInstance = new Cron((signal) => {
-      log.info("DB optimize runing");
-
-      try {
-        db.exec("pragma optimize");
-
-        log.info("DB optimize completed");
-      } catch (error) {
-        log.error("DB optimize failed", { error });
-      }
-
-      if (!signal.aborted) {
-        log.info(`Next db optimize at ${cronInstance.nextAt()}`);
-      }
-    }, {
-      when: "0 * * * *",
-      timezone: "UTC",
-      tickerTimeout: 300,
-    });
-
-    log.info(`Next db optimize at ${cronInstance.nextAt()}`);
-
-    cronInstance.start();
-
-    return cronInstance;
-  },
-  shutdown: (cron) => cron.stop(),
 });
 
 processLifecycle.registerService({
