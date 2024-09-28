7648dfaa7303d0d10f981624ab5846c3ea545e75
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-09-28 12:11:21 +0100

    Use replace @std/archive with @std/tar
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/api/routes/notes/export.ts
+++ src/apps/api/routes/notes/export.ts
@@ -1,15 +1,13 @@
 import type { Hono } from "@hono/hono";
 import { sql } from "@m4rc3l05/sqlite-tag";
-import { Tar } from "@std/archive";
-import { Buffer } from "@std/io/buffer";
-import { toReadableStream } from "jsr:@std/io@^0.224.0/to-readable-stream";
+import { TarStream, type TarStreamInput } from "@std/tar/tar-stream";
 import type { NotesTable } from "#src/database/types/mod.ts";
 
 export const exportNotes = (app: Hono) => {
   app.get("/export", (c) => {
-    const tar = new Tar();
     const textEncoder = new TextEncoder();
     const notes = c.get("db").iter<NotesTable>(sql`select * from notes`);
+    const input: TarStreamInput[] = [];
 
     for (const note of notes) {
       const encoded = textEncoder.encode(note.content);
@@ -17,15 +15,21 @@ export const exportNotes = (app: Hono) => {
         ? `/trashed/${note.name}.txt`
         : `/notes/${note.name}.txt`;
 
-      tar.append(tarPath, {
-        reader: new Buffer(encoded),
-        contentSize: encoded.byteLength,
+      input.push({
+        readable: ReadableStream.from([encoded]),
+        path: tarPath,
+        size: encoded.byteLength,
+        type: "file",
       });
     }
 
-    return c.body(toReadableStream(tar.getReader()), 200, {
-      "content-type": "application/x-tar",
-      "content-disposition": 'attachment; filename="notes.tar"',
-    });
+    return c.body(
+      ReadableStream.from(input).pipeThrough(new TarStream()),
+      200,
+      {
+        "content-type": "application/x-tar",
+        "content-disposition": 'attachment; filename="notes.tar"',
+      },
+    );
   });
 };
